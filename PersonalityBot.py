67018d34c3e2e5aa8e26d87f4bc7600f54ccfc65
Syed Raza Abbas <you@example.com>
2019-05-05 23:50:41 -0700

    Added responses for wrong queries

--- PersonalityBot.py
+++ PersonalityBot.py
@@ -12,18 +12,28 @@ responses = {
     "Ap ka naam kia ha?": [
         "Allah Deta",
         "Naa cheez ko Allah Deta kehty hn",
-    ]
+        "A girl has no name"
+    ],
+    "Ziada shaloo paloo hony ka zrort nai ha": "he he. mazak tha.."
 }
+
+wrongQueries = [
+    "Arey kehna kia chaty hoe?..",
+    "I beg your pardon",
+    "Ap pagal ho kia?"
+]
 
 
 def respond(message):
     if message in responses:
         return random.choice(responses[message])
+    else:
+        return random.choice(wrongQueries)
 
 
 if __name__ == "__main__":
     print("Muqaalma shuru kren..")
-    message = input()
+    message = input("USER:")
     while message != "AllahHafiz":
-        print(respond(message))
+        print("BOT:", respond(message))
         message = input()
